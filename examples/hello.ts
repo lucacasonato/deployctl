5f83cade1cde01a63eaa6d6bfd81cc38f4977197
Satya Rohith <me@satyarohith.com>
2022-02-09 18:19:31 +0530

    try

--- examples/hello.ts
+++ examples/hello.ts
@@ -2,8 +2,8 @@ import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 
 async function handler(_req: Request) {
   const text = await Deno.readTextFile(new URL(import.meta.url));
-  return new Response(text, {
-    headers: { "content-type": "text/plain; charset=utf8" },
+  return new Response(JSON.stringify(Deno.env.toObject(), null, 2), {
+    headers: { "content-type": "application/json; charset=utf8" },
   });
 }
 
